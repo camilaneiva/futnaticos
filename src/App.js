e48f045862fea0d41d2d47574e3cbdba60c2e6e2
Camila Neiva <camilaneivamartins@gmail.com>
2022-04-04 13:51:50 -0300

    check

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
+import { ThemeProvider } from 'styled-components'
+import { theme } from './design-system/theme'
 import { Menu } from './components/Menu'
-import { TableRow } from './components/TableRow'
+import { Table } from './components/Table'
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       {/* <Menu /> */}
-
-      <TableRow />
-    </>
+      <Table />
+    </ThemeProvider>
   )
 }
 
