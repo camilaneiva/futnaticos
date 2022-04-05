a1a31327e3827aa05211654bad2982b2497d3f6b
Camila Neiva <77302356+camilaneiva@users.noreply.github.com>
2022-04-05 12:44:23 -0300

    Revert "Componente Tabela (#3)"
    This reverts commit 2e8ba70a41728608c74ab7373bfae1c822458056.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
 import { ThemeProvider } from 'styled-components'
-import { theme } from './design-system/theme'
 import { Menu } from './components/Menu'
-import { Table } from './components/Table'
-import clubSymbol from './imgs/symbol.svg'
-
-const tableData = [
-  {
-    club: 'Manchester United',
-    results: ['l', 'w', 'w', 'w', 'd'],
-    position: 1,
-    points: 50,
-    played: 29,
-    won: 14,
-    drawn: 8,
-    lost: 7,
-    goalsFor: 48,
-    goalsAgainst: 40,
-    goalsDifference: 8,
-    clubSymbol: clubSymbol
-  }
-]
+import { theme } from './design-system/theme'
 
 export const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      {/* <Menu /> */}
-      <Table data={tableData} />
+      <Menu />
     </ThemeProvider>
   )
 }
