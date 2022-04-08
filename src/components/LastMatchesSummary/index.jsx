adb602b38a35ed4f7fc27829757687f80dee3865
Camila Neiva <camilaneivamartins@gmail.com>
2022-04-08 13:05:17 -0300

    Arrumando erros de console.

--- src/components/LastMatchesSummary/index.jsx
+++ src/components/LastMatchesSummary/index.jsx
@@ -9,13 +9,13 @@ import { IconCheck, IconX, IconMinus, Wrapper } from './style'
 export const LastMatchesSummary = ({ results }) => {
   return (
     <Wrapper>
-      {results.map((result) => {
+      {results.map((result, index) => {
         return (
-          <>
+          <React.Fragment key={index}>
             {result === 'w' && <IconCheck icon={faCircleCheck} />}
             {result === 'l' && <IconX icon={faCircleXmark} />}
             {result === 'd' && <IconMinus icon={faCircleMinus} />}
-          </>
+          </React.Fragment>
         )
       })}
     </Wrapper>
